f4445772936d23eb982e6222e3c320b9d54ec2e5
no13bus <no13bus@gmail.com>
2015-01-21 21:46:53 +0800

    up

--- example/main.go
+++ example/main.go
@@ -9,37 +9,37 @@ func main() {
 	// var nodes Nodes
 	nodes, err := rubychina.GetNodes()
 	if err != nil {
-		fmt.Printf("node error:%s" % err)
+		fmt.Println(err)
 		return
 	}
 	topics, err := rubychina.GetTopics()
 	if err != nil {
-		fmt.Printf("topics error:%s" % err)
+		fmt.Println(err)
 		return
 	}
 	users, err := rubychina.GetUsers()
 	if err != nil {
-		fmt.Printf("users error:%s" % err)
+		fmt.Println(err)
 		return
 	}
 	user, err := rubychina.GetUserByName("no13bus")
 	if err != nil {
-		fmt.Printf("userbyname error:%s" % err)
+		fmt.Println(err)
 		return
 	}
 	topic, err := rubychina.GetTopicByID(1)
 	if err != nil {
-		fmt.Printf("GetTopicByID error:%s" % err)
+		fmt.Println(err)
 		return
 	}
 	sites, err := rubychina.GetSites()
 	if err != nil {
-		fmt.Printf("GetSites error:%s" % err)
+		fmt.Println(err)
 		return
 	}
 	fa_topics, err := rubychina.GetFavoriteTopic("no13bus")
 	if err != nil {
-		fmt.Printf("GetFavoriteTopic error:%s" % err)
+		fmt.Println(err)
 		return
 	}
 
